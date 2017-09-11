0a2fc6199dcb9b6adf1af00dd8c196da62da2583
Jon Wyatt <jon@zoetrope.io>
2017-09-11 09:10:09 +0100

    Fix <Login/> export

--- src/containers/auth/index.js
+++ src/containers/auth/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
+export { default as Login } from './Login'
 
-export const Login = props => (<div>
-  <h3>Login</h3>
-  <pre>{JSON.stringify(props, null, 2)}</pre>
-</div>)
 
 export const Register = props => (<div>
   <h3>Register</h3>
