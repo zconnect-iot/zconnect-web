403764e0cc481aa8616ffe52b0ddf7252c88c3fa
coxm <coxm@users.noreply.github.com>
2017-09-15 10:34:14 +0100

    Modify StatCard API

--- widgets/StatCard/index.js
+++ widgets/StatCard/index.js
@@ -1,97 +1,46 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
+import BEMHelper from 'react-bem-helper'
 
-import defaultStyles from './style.scss'
-
-export const DELTA_UP = '+'
-export const DELTA_DOWN = '-'
-
-const itemPropType = PropTypes.shape({
-  figure: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  desc: PropTypes.string.isRequired,
-  deltaType: PropTypes.oneOf([DELTA_UP, DELTA_DOWN]),
-  deltaContent: PropTypes.oneOfType([
-    PropTypes.string.isRequired,
-    PropTypes.number,
-  ]),
-})
-
-const renderDelta = (item, deltaClass) => {
-  if (!item.deltaContent)
-    return null
-
-  const classes = [deltaClass]
-  switch (item.deltaType) {
-    case DELTA_UP:
-      classes.push(defaultStyles.deltaUp)
-      break
-    case DELTA_DOWN:
-      classes.push(defaultStyles.deltaDown)
-      break
-    default:
-      break
-  }
-
-  return <small className={classNames(classes)}>{item.deltaContent}</small>
-}
+import './style.scss'
 
 /**
  * Stat card component for displaying large, simple metrics.
- *
- * @example
- * // A stat card with a number and a description.
- * <StatCard item={{ stat: 12, desc: 'Cups of coffee', }}/>
- *
- * @example
- * // A stat card with a number and description, along with a 'delta'
- * // indicating a 'success'. Alternatives are 'warning', 'danger', 'info'.
- * <StatCard item={{
- *   stat: 12,
- *   desc: 'Coffees',
- *   deltaType: 'success',
- *   deltaContent: '25%'
- * }}/>
- *
- * @example
- * // A stat card with multiple items.
- * <StatCard items={[
- *   { stat: 12, desc: 'Coffees', deltaType: 'success', deltaContent: '25%' },
- *   { stat: 8, desc: 'Teas', deltaType: 'danger', deltaContent: '25%' },
- *   { stat: 1, desc: 'Juices' }
- * ]}/>
  */
-export default class StatCard extends Component {
-  static propTypes = {
-    item: itemPropType,
-    items: PropTypes.arrayOf(itemPropType),
-    styleNames: PropTypes.shape({
-      statcard: PropTypes.object,
-      figure: PropTypes.object,
-      desc: PropTypes.object,
-      delta: PropTypes.object,
-    }),
-  }
+export const classes = new BEMHelper('StatCard')
 
-  renderItem = (item, styleNames) => {
-    const classes = Object.assign({}, styleNames, item.styleNames)
-    return (<div key={item.key || item.desc}>
-      <span className={classes.desc}>{item.desc}</span>
-      <div className={classes.figure}>
-        {item.figure}
-        {renderDelta(item, classes.delta)}
+const StatCard = props => {
+  let delta = null
+  if (props.delta) {
+    const down = props.delta < 0
+    delta = <span {...classes('delta', {
+      down,
+      danger: down === !props.reverseColor,
+    })}>
+      {props.delta}{props.deltaUnits}
+    </span>
+    console.log('StatCard', props, {down, danger: down || props.reverseColor})
+  }
+  return <div {...classes()}>
+    <div>
+      <span {...classes('description')}>{props.description}</span>
+      <div>
+        <div {...classes('figure')}>
+          {props.figure}
+          {delta}
+        </div>
       </div>
-    </div>)
-  }
-
-  render() {
-    const styleNames = Object.assign({}, defaultStyles, this.props.styleNames)
-    if (this.props.item)
-      return (<div className={styleNames.statcard}>
-        {this.renderItem(this.props.item, styleNames)}
-      </div>)
-    return (<div className={styleNames.statcard}>
-      {this.props.items.map(item => this.renderItem(item, styleNames))}
-    </div>)
-  }
+    </div>
+  </div>
 }
+
+StatCard.propTypes = {
+  figure: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  description: PropTypes.string.isRequired,
+  delta: PropTypes.number,
+  reverseColor: PropTypes.bool,
+  deltaUnits: PropTypes.string,
+}
+
+export default StatCard
