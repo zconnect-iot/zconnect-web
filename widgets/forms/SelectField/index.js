0d3c279da641f1d3a5555359a7ab87d990426657
coxm <coxm@users.noreply.github.com>
2017-09-15 17:21:51 +0100

    Add SelectField

---
+++ widgets/forms/SelectField/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Field } from 'redux-form/immutable'
+import BEMHelper from 'react-bem-helper'
+import uniqueId from 'lodash/uniqueId'
+
+import {
+  genericError,
+  genericWarning,
+  innerPropTypes,
+  propTypes,
+  defaultProps,
+} from '../genericInput/index'
+
+import './style.scss'
+
+const classes = BEMHelper('SelectField')
+
+export const renderInner = (props) => {
+  const { touched, error, warning } = props.meta
+  const selectId = uniqueId('SelectField__input')
+  return (<div {...classes()}>
+    <label
+      htmlFor={selectId}
+      {...classes('label', {
+        checked: props.input.checked,
+        disabled: props.disabled,
+      })}
+    >
+      {props.label}
+    </label>
+
+    <select
+      id={selectId}
+      placeholder={props.placeholder || props.label}
+      {...classes('select')}
+      {...props.input}
+    >
+      {props.children}
+    </select>
+
+    {touched && (
+      (error && genericError(classes, error)) ||
+      (warning && genericWarning(classes, warning))
+    )}
+  </div>)
+}
+
+renderInner.propTypes = innerPropTypes
+
+const component = props => (<Field
+  type="checkbox"
+  component={renderInner}
+  {...props}
+/>)
+
+export default component
+
+component.propTypes = propTypes
+component.defaultProps = defaultProps
