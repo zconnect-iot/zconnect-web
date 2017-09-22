aef0655bf9d04275bd5f10afe32d542c908e21e5
coxm <coxm@users.noreply.github.com>
2017-09-22 12:29:56 +0100

    Add tab docs

--- views/Tabs/index.js
+++ views/Tabs/index.js
@@ -5,6 +5,33 @@ import { TabLink as Link, TabContent as Content } from 'react-tabs-redux'
 
 import './style.scss'
 
+/**
+ * Tabs component.
+ *
+ * This module provides simple wrappers to the `react-tabs-redux` tab
+ * components for ease of styling. Default styles are in {@link ./style.scss}.
+ *
+ * @example
+ * <Tabs>
+ *   <TabLinkList>
+ *     <TabLink to="atRisk"><span>At risk</span></TabLink>
+ *     <TabLink to="needsData"><span>Needs data</span></TabLink>
+ *   </TabLinkList>
+ *
+ *   <TabContent for="atRisk">
+ *     <h2>At risk content</h2>
+ *     <p>These buildings are at risk.</p>
+ *   </TabContent>
+ *
+ *   <TabContent for="needsData">
+ *     <h2>Needs data collection content</h2>
+ *     <p>These buildings need data collection.</p>
+ *   </TabContent>
+ * </Tabs>
+ *
+ * Alternatively the components from `react-tabs-redux` can be used in place of
+ * any here. @see {@link https://github.com/patrik-piskay/react-tabs-redux}.
+ */
 export { Tabs } from 'react-tabs-redux'
 
 export const classes = new BEMHelper('Tabs')
