d03e02fed11aa055e0a8ed794762887eb4fef133
coxm <coxm@users.noreply.github.com>
2017-10-05 13:43:00 +0100

    Add GriddleViewLink docs

--- widgets/GriddleViewLink/index.js
+++ widgets/GriddleViewLink/index.js
@@ -6,6 +6,19 @@ import './style.scss'
 
 const classes = new BEMHelper('GriddleViewLink')
 
+/**
+ * Simple 'View' link which can be used in a Griddle table with a URL column.
+ *
+ * @example
+ * // Using a Griddle ColumnDefinition:
+ * <ColumnDefinition
+ *   id="link"
+ *   key="link"
+ *   title="Link"
+ *   customComponent={GriddleViewLink}
+ * />,
+ *
+ */
 export default ({ value, className }) => (
   <Link to={value} {...classes(null, null, className)}>
     View
