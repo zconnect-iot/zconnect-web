1aa68eb21ab0f388db15f612fd35a5fd87dc0422
Jon Wyatt <jon@zoetrope.io>
2017-10-05 14:45:16 +0100

    #17 Add Basic forgotten password component

--- containers/auth/Login/index.js
+++ containers/auth/Login/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import BEMHelper from 'react-bem-helper'
+import { formValueSelector } from 'redux-form/immutable'
 
-import { login } from 'zc-core/auth/actions'
+import { isValidEmail } from 'zc-core/auth/utils'
+import { login, loginError } from 'zc-core/auth/actions'
 import { selectLoginAPIState, selectLoginErrorMessage } from 'zc-core/auth/selectors'
 import { toJS } from 'zc-core/hocs'
 
@@ -13,14 +15,20 @@ import { Logo } from '../../../components'
 import './style.scss'
 
 const classes = BEMHelper({ name: 'Login' })
+const selectFormState = formValueSelector('loginForm')
 
 class Login extends React.Component {
   handleSubmit = (payload) => {
     const { email, password } = payload.toJS()
-    this.props.login(email, password)
+    if (!isValidEmail(email)) return this.props.registerError('emailinvalid')
+    if (password.length < 8) return this.props.registerError('passwordinvalid')
+    return this.props.login(email, password)
   }
-  handleForgotten = () => this.props.history.push('/forgotten')
-
+  handleForgotten = () => {
+    const { history, email } = this.props
+    const queryString = email ? `?email=${encodeURIComponent(email)}` : ''
+    history.push(`/forgotten${queryString}`)
+  }
   render() {
     const { api, errorMessage } = this.props
     const { t } = this.context
@@ -50,6 +58,8 @@ Login.contextTypes = {
 
 Login.propTypes = {
   login: PropTypes.func.isRequired,
+  registerError: PropTypes.func.isRequired,
+  email: PropTypes.string,
   api: PropTypes.shape({
     error: PropTypes.bool.isRequired,
     pending: PropTypes.bool.isRequired,
@@ -63,15 +73,18 @@ Login.propTypes = {
 
 Login.defaultProps = {
   errorMessage: '',
+  email: null,
 }
 
 const mapStateToProps = state => ({
   api: selectLoginAPIState(state),
   errorMessage: selectLoginErrorMessage(state),
+  email: selectFormState(state, 'email'),
 })
 
 const mapDispatchToProps = dispatch => ({
   login: (email, password) => dispatch(login(email, password)),
+  registerError: e => dispatch(loginError({ title: e })),
 })
 
 export default connect(
--- containers/auth/index.js
+++ containers/auth/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 
 export { default as Login } from './Login'
-
+export { default as Forgotten } from './Forgotten'
 
 export const Register = props => (<div>
   <h3>Register</h3>
-  <pre>{JSON.stringify(props, null, 2)}</pre>
-</div>)
-
-export const Forgotten = props => (<div>
-  <h3>Forgotten</h3>
   <pre>{JSON.stringify(props, null, 2)}</pre>
 </div>)
