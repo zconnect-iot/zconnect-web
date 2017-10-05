25d818197e14f12ecca31059871b9f3abd71bae2
coxm <coxm@users.noreply.github.com>
2017-10-05 13:38:30 +0100

    Remove unused List TableBody component

--- views/List/GriddlePlugin.js
+++ views/List/GriddlePlugin.js
@@ -2,8 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { plugins } from 'griddle-react'
 import { connect } from 'react-redux'
-import stylePropType from 'react-style-proptype'
-import { List as ImmutableList } from 'immutable'
 
 import './style.scss'
 
@@ -42,25 +40,8 @@ export const Row = connect((state, props) => ({
   rowData: plugins.LocalPlugin.selectors.rowDataSelector(state, props),
 }))(renderRow)
 
-/** Default list table body component. */
-export const TableBody = ({ rowIds, Row: TRow, style, className }) => (
-  <tbody style={style} className={className}>
-    {rowIds && rowIds.map(rowId => <TRow key={rowId} griddleKey={rowId} />)}
-  </tbody>
-)
-TableBody.propTypes = {
-  rowIds: PropTypes.instanceOf(ImmutableList).isRequired,
-  Row: PropTypes.func.isRequired,
-  style: stylePropType,
-  className: PropTypes.string.isRequired,
-}
-TableBody.defaultProps = {
-  style: null,
-}
-
 export const components = Object.assign({}, plugins.LocalPlugin.components, {
   Row,
-  TableBody,
 })
 
 export default Object.assign({}, plugins.LocalPlugin, {
