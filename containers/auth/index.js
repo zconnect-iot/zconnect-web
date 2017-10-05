6fac840613d5ec7621379f49f0a285af7266f73d
Jon Wyatt <jon@zoetrope.io>
2017-10-05 15:57:42 +0100

    #17 Add Register component

--- containers/auth/index.js
+++ containers/auth/index.js
@@ -1,9 +1,3 @@
-import React from 'react'
-
 export { default as Login } from './Login'
 export { default as Forgotten } from './Forgotten'
-
-export const Register = props => (<div>
-  <h3>Register</h3>
-  <pre>{JSON.stringify(props, null, 2)}</pre>
-</div>)
+export { default as Register } from './Register'
