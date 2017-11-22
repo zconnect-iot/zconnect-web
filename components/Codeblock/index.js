8c5ad9d7b59da2a27c0a0325f38baf579a307935
Jon Wyatt <jon@zoetrope.io>
2017-11-22 09:52:59 +0000

    Codeblock - Switch to googlecode style and remove as yet unused languages

--- components/Codeblock/index.js
+++ components/Codeblock/index.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter/light'
-import js from 'react-syntax-highlighter/languages/hljs/javascript'
 import json from 'react-syntax-highlighter/languages/hljs/json'
-import python from 'react-syntax-highlighter/languages/hljs/python'
-import { docco } from 'react-syntax-highlighter/styles/hljs'
+import { googlecode } from 'react-syntax-highlighter/styles/hljs'
 
-registerLanguage('javascript', js)
 registerLanguage('json', json)
-registerLanguage('python', python)
 
 export default function Codeblock({ dictionary, children, language, ...props }) {
   const codeString = children || JSON.stringify(dictionary, null, '  ')
   return (
     <div className="Codeblock">
-      <SyntaxHighlighter language={language} style={docco} {...props}>
+      <SyntaxHighlighter language={language} style={googlecode} {...props}>
         {codeString}
       </SyntaxHighlighter>
     </div>
@@ -28,9 +24,7 @@ Codeblock.propTypes = {
     PropTypes.node,
   ]),
   language: PropTypes.oneOf([
-    'javascript',
     'json',
-    'python',
   ]),
 }
 
