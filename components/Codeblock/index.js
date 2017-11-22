a3b5d73081e1c96aaf6001daf83bd5d89a3cfdd0
Jon Wyatt <jon@zoetrope.io>
2017-11-22 15:13:14 +0000

    Codeblock - Remove react-syntax-highlighter

--- components/Codeblock/index.js
+++ components/Codeblock/index.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import SyntaxHighlighter, { registerLanguage } from 'react-syntax-highlighter/light'
-import json from 'react-syntax-highlighter/languages/hljs/json'
-import { googlecode } from 'react-syntax-highlighter/styles/hljs'
 
-registerLanguage('json', json)
+import './styles.scss'
 
-export default function Codeblock({ dictionary, children, language, ...props }) {
+
+export default function Codeblock({ dictionary, children }) {
   const codeString = children || JSON.stringify(dictionary, null, '  ')
   return (
     <div className="Codeblock">
-      <SyntaxHighlighter language={language} style={googlecode} {...props}>
+      <pre className="Codeblock__pre">
         {codeString}
-      </SyntaxHighlighter>
+      </pre>
     </div>
   )
 }
@@ -23,13 +21,9 @@ Codeblock.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
-  language: PropTypes.oneOf([
-    'json',
-  ]),
 }
 
 Codeblock.defaultProps = {
   dictionary: null,
   children: null,
-  language: 'json',
 }
