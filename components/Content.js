534d30e4006cc8814a801a0417dcef9382af6114
Jon Wyatt <jon@zoetrope.io>
2017-12-19 14:39:27 +0000

    Make Page flex and full height with Content as flex 1 row

--- components/Content.js
+++ components/Content.js
@@ -9,27 +9,29 @@ const classes = BEMHelper({ name: 'Content' })
 
 export default function Content({ title, subtitle, actionItems, children, className }) {
   return (
-    <Col {...classes(null, null, className)} xs={12}>
-      <Row {...classes('header')}>
-        <Col {...classes('headerCenter')} xs={12} lgOffset={1} lg={10}>
-          <div {...classes('title')}>
-            <h4>{title}</h4>
-            <h6>{subtitle}</h6>
-          </div>
-          {actionItems.map(item => (
-            <Button {...classes('button')} key={item.title} hollow {...item}>
-              <Icon name={item.icon} size={24} />
-              {item.title}
-            </Button>
-          ))}
-        </Col>
-      </Row>
-      <Row {...classes('body')}>
-        <Col xs={12} lgOffset={1} lg={10}>
-          {children}
-        </Col>
-      </Row>
-    </Col>
+    <Row {...classes(null, null, className)}>
+      <Col xs={12}>
+        <Row {...classes('header')}>
+          <Col {...classes('headerCenter')} xs={12} lgOffset={1} lg={10}>
+            <div {...classes('title')}>
+              <h4>{title}</h4>
+              <h6>{subtitle}</h6>
+            </div>
+            {actionItems.map(item => (
+              <Button {...classes('button')} key={item.title} hollow {...item}>
+                <Icon name={item.icon} size={24} />
+                {item.title}
+              </Button>
+            ))}
+          </Col>
+        </Row>
+        <Row {...classes('body')}>
+          <Col xs={12} lgOffset={1} lg={10}>
+            {children}
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   )
 }
 
