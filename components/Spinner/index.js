5b6bce62827520d2b8c1f172462c2887c5609cc4
Jon Wyatt <jon@zoetrope.io>
2018-01-08 11:57:49 +0000

    Spinner - Let Spinner render optional children

--- components/Spinner/index.js
+++ components/Spinner/index.js
@@ -7,13 +7,14 @@ import './style.scss'
 const classes = BEMHelper({ name: 'Spinner' })
 
 export default function Spinner(props) {
-  const { className, full } = props
+  const { className, full, children } = props
   const size = full ? 100 : props.size
   return (
     <div {...classes(null, full && 'full', className)}>
       <div className="lds-dual-ring" style={{ width: `${size}px`, height: `${size}px` }}>
         <div style={{ borderWidth: `${size / 20}px` }} />
       </div>
+      {children}
     </div>
   )
 }
@@ -22,10 +23,15 @@ Spinner.propTypes = {
   size: PropTypes.number,
   full: PropTypes.bool,
   className: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
 }
 
 Spinner.defaultProps = {
   size: 30,
   full: false,
   className: '',
+  children: null,
 }
