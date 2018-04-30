a90045e67cb87e41f80f980d1365b4453400d994
Jon Wyatt <jon@zoetrope.io>
2018-04-30 15:58:33 +0100

    Add Address component

---
+++ components/Address/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+export default function Address(props) {
+  const { street_address, locality, region, poboxno, postalcode, country } = props
+  return (<div>
+    {street_address && <p>{street_address}</p>}
+    {locality && <p>{locality}</p>}
+    {region && <p>{region}</p>}
+    {poboxno && <p>{poboxno}</p>}
+    {postalcode && <p>{postalcode}</p>}
+    {country && <p>{country}</p>}
+  </div>)
+}
+
+Address.propTypes = {
+  street_address: PropTypes.string,
+  locality: PropTypes.string,
+  region: PropTypes.string,
+  poboxno: PropTypes.string,
+  postalcode: PropTypes.string,
+  country: PropTypes.string,
+}
+
+Address.defaultProps = {
+  street_address: '',
+  locality: '',
+  region: '',
+  poboxno: '',
+  postalcode: '',
+  country: '',
+}
