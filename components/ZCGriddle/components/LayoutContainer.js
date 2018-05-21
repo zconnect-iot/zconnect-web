e4e740881ff83841dc465770f736a26c78e483a4
Jon Wyatt <jon@zoetrope.io>
2018-05-21 15:29:11 +0100

    Hide pagination if only one page for both internally and externally controlled griddles plus...
    List uses ZCGriddle to benefit from pagination hiding logic

--- components/ZCGriddle/components/LayoutContainer.js
+++ components/ZCGriddle/components/LayoutContainer.js
@@ -14,6 +14,12 @@ const EnhancedLayout = OriginalComponent => compose(
     state => ({
       className: plugins.LocalPlugin.selectors.classNamesForComponentSelector(state, 'Layout'),
       style: plugins.LocalPlugin.selectors.stylesForComponentSelector(state, 'Layout'),
+      pageSize: plugins.LocalPlugin.selectors.pageSizeSelector(state),
+      // When griddle is controlled recordCount is passed as a prop and present in state
+      // otherwise the filteredDataSelector can be used to determine whether pagination
+      // should be shown
+      recordCount: state.getIn(['pageProperties', 'recordCount']) ||
+        plugins.LocalPlugin.selectors.filteredDataSelector(state).size,
       title: state.get('title'),
       hidePagination: state.get('hidePagination'),
       hideFilter: state.get('hideFilter'),
@@ -28,7 +34,7 @@ const EnhancedLayout = OriginalComponent => compose(
     className: props.className,
     style: props.style,
     title: props.title,
-    hidePagination: props.hidePagination,
+    hidePagination: props.hidePagination || props.recordCount < props.pageSize,
     hideFilter: props.hideFilter,
   })),
 )(OriginalComponent)
