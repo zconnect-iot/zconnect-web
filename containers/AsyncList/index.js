b07742abc65e735be0b388fb5047cd31aa546901
Martin Rolph <martin@zoetrope.io>
2018-06-18 17:16:41 +0100

    Revert "Async list"
    This reverts commit 8ea27f32047984d5458333f6a1ad63b398c728b7.

--- containers/AsyncList/index.js
+++ containers/AsyncList/index.js
@@ -1,5 +1,22 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+/*
+  AsyncList interfaces with zconnect v2 django paginated results endpoints. This is
+  a controlled component so will just fetch and display the currentPage at the pageSize
+  specified in props.
+
+  You need to maintain the state manually via redux or url search params or use
+  AsyncListWithState makes the component stateful and provides an events object used by
+  the griddle with functions that update that state
+
+  Required props:
+    endpoint - the name of the endpoint config which should use the storeMethod in utils
+    storeKey - the storeKey defined inside the endpoint config
+    pageSize - number - defaults to 10
+    currentPage - number - defaults to 1
+
+  Passing hidePagination and hideFilter props it's possible to make this a controlled
+  component and handle pagination, sorting and filtering outside of Griddle
+*/
+
 import { connect } from 'react-redux'
 
 import { toJS } from 'zc-core/hocs'
@@ -8,7 +25,7 @@ import { selectAPIState } from 'zc-core/api/selectors'
 
 import { selectResults, selectPageProperties, selectErrorMessage } from './selectors'
 
-import { AsyncList as Unconnected } from './AsyncList'
+import AsyncList from './AsyncList'
 
 const mapStateToProps = (state, props) => ({
   data: selectResults(state, props),
@@ -43,36 +60,4 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps,
   mergeProps,
-)(toJS(Unconnected))
-
-/**
- * AsyncList interfaces with zconnect v2 django paginated results endpoints. This is
- * a controlled component so will just fetch and display the currentPage at the pageSize
- * specified in props.
- *
- * You need to maintain the state manually via redux or url search params or use
- * AsyncListWithState makes the component stateful and provides an events object used by
- * the griddle with functions that update that state
- *
- * Passing hidePagination and hideFilter props it's possible to make this a controlled
- * component and handle pagination, sorting and filtering outside of Griddle
-*/
-// export default function AsyncList({ ...props }) {
-//   return <Connected {...props} />
-// }
-
-// AsyncList.propTypes = {
-//   /** The name of the endpoint config which should use the storeMethod in utils */
-//   endpoint: PropTypes.string.isRequired,
-//   * the storeKey defined inside the endpoint config 
-//   storeKey: PropTypes.string.isRequired,
-//   /** How many items to show per page */
-//   pageSize: PropTypes.number,
-//   /** Which page to show */
-//   currentPage: PropTypes.number,
-// }
-
-// AsyncList.defaultProps = {
-//   pageSize: 10,
-//   currentPage: 1,
-// }
+)(toJS(AsyncList))
