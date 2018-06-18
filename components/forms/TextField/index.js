ef41b08fe00be27e3b6cc3e70893edc4afe8c58c
Martin Rolph <martin@zoetrope.io>
2018-06-18 16:59:42 +0100

    Text field

--- components/forms/TextField/index.js
+++ components/forms/TextField/index.js
@@ -1,11 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import { createFieldComponent } from '../genericInput/index'
 
 import './style.scss'
 
 const { component, renderInput, classes } = createFieldComponent('TextField', 'text')
 
+function TextField({ ...props }) {
+  // You can't just render <component ... />, it doesn't gert parsed correctly
+  const Field = component
+  return <Field {...props} />
+}
+
+TextField.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  validate: PropTypes.arrayOf(PropTypes.func),
+  autocomplete: PropTypes.bool,
+}
+
+TextField.defaultProps = {
+  validate: [],
+  autocomplete: false,
+}
+
 export {
-  component as default,
+  TextField as default,
   renderInput,
   classes,
 }
