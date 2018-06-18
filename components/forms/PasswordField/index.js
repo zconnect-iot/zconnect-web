78603405160db3742367e06a775e87c2f6a2e518
Martin Rolph <martin@zoetrope.io>
2018-06-18 16:59:20 +0100

    Password field

--- components/forms/PasswordField/index.js
+++ components/forms/PasswordField/index.js
@@ -1,11 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import { createFieldComponent } from '../genericInput/index'
 
 import './style.scss'
 
 const { component, renderInput, classes } = createFieldComponent('PasswordField', 'password')
 
+function PasswordField({ ...props }) {
+  // You can't just render <component ... />, it doesn't gert parsed correctly
+  const Field = component
+  return <Field {...props} />
+}
+
+PasswordField.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  validate: PropTypes.arrayOf(PropTypes.func),
+  autocomplete: PropTypes.bool,
+}
+
+PasswordField.defaultProps = {
+  validate: [],
+  autocomplete: false,
+}
+
 export {
-  component as default,
+  PasswordField as default,
   renderInput,
   classes,
 }
