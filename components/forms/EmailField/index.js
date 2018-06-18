199ffc2ed77c91bf21d16f59e57a636b24b7d2e3
Martin Rolph <martin@zoetrope.io>
2018-06-18 16:59:01 +0100

    Email field

--- components/forms/EmailField/index.js
+++ components/forms/EmailField/index.js
@@ -1,11 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import { createFieldComponent } from '../genericInput/index'
 
 import './style.scss'
 
 const { component, renderInput, classes } = createFieldComponent('EmailField', 'email')
 
+function EmailField({ ...props }) {
+  // You can't just render <component ... />, it doesn't gert parsed correctly
+  const Field = component
+  return <Field {...props} />
+}
+
+EmailField.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  validate: PropTypes.arrayOf(PropTypes.func),
+  autocomplete: PropTypes.bool,
+}
+
+EmailField.defaultProps = {
+  validate: [],
+  autocomplete: false,
+}
+
 export {
-  component as default,
+  EmailField as default,
   renderInput,
   classes,
 }
